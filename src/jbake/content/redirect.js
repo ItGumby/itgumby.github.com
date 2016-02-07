1a711e0931e7b00c42108a9a0539b42e55bcf412
Brian Street <brian.c.street@gmail.com>
2016-02-06 23:37:35 -0700

    resolved JS redirect bugs

--- src/jbake/content/redirect.js
+++ src/jbake/content/redirect.js
@@ -1,20 +1,12 @@
-;
-// trim octopress trailing slash, paths & folders, and any leading dates
-var reqPage = window.location.pathname.replace(/\/$/, '').replace(/^.*\//, '').replace(/^[-0-9]*/, '').replace(/\.[a-z]+/, '');
-if (reqPage == '404.html') reqPage = 'blog/2014/02/cq-defineObjects-cheatsheet/';
-console && console.log('request = ' + reqPage);
-if (reqPage.length) {
-  var links = document.getElementsByTagName('a');
-  var hrefs = [];
-  for (var i=0; i< links.length; i++) {
-    if (links[i]['href'].match(reqPage)) { hrefs.push(links[i]['href']); }
+function redirect() {
+  // trim octopress trailing slash, paths & folders, and any leading dates
+  var matchPath = window.location.pathname.replace(/\/$/, '').replace(/^.*\//, '');
+  var dest = document.querySelector('.container a[href*="' + matchPath + '"]');
+  if (dest) {
+    console && console.log('redirecting from: ' + window.location.pathname + ' to ' + dest['href']);
+    window.location.replace(dest['href']);
   }
-  if (hrefs.length == 1) {
-    console.log('redirect to: ' + hrefs[0]);
-    //window.location.replace(hrefs[0]);
-  } else {
-    console.log('matching links: ' + hrefs);
-  }
-} else {
-  console && console.log('no page extracted from ' + window.location.pathname);
-};
+}
+
+if (document.readyState != 'loading') redirect();
+else document.addEventListener('DOMContentLoaded', redirect);
